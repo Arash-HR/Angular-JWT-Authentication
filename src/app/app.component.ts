3abd09946a4ac86734e4f957aef254f776952e15
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-13 12:27:35 +0330

    clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'auth';
-}
+export class AppComponent { }
