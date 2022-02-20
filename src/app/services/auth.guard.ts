6bb7081b8afbd818716c57cfe54064d8d8a4f030
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-21 00:48:04 +0330

    change function for check logged in

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -8,11 +8,13 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  private loggedIn!: boolean;
   constructor(private authService: AuthService, private router: Router) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.authService.isLoggedIn()) {
+    this.authService.authStatus.subscribe(status => this.loggedIn = status);
+    if (!this.loggedIn) {
       this.router.navigate(['/login']); // go to login if not authenticated
       return false;
     }
