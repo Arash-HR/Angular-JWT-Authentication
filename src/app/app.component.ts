63736cdb7b675842c8176dbf53640edbfd247579
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-21 00:51:57 +0330

    auth status and logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = "JWT Authentication";
+  public loggedIn!: boolean;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.authStatus.subscribe(status => this.loggedIn = status);
+  }
+
+  logOut() {
+    this.authService.logOut();
+  }
 }
