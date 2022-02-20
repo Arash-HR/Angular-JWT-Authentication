2d66dca3176356ae9540cda5b94de4ce2e1fab2c
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-21 00:53:31 +0330

    handle login response

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,18 +12,33 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  error = null;
   public loginFormData: User = {
     email: null,
     password: null
   }
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    this.authService.signin(this.loginFormData).subscribe((data) => (console.log(data)));
+    // this.authService.signin(this.loginFormData).subscribe((data) => (console.log(data)));
+    this.authService.signIn(this.loginFormData).subscribe(
+      data => { this.handleResponse(data) },
+      error => { this.error = error.error.message }
+    );
+  }
+
+  handleResponse(data: any) {
+    this.localStorage.set("access_token", data.access_token);
+    this.authService.changeAuthStatus(true);
+    this.router.navigateByUrl('/');
   }
 
 }
