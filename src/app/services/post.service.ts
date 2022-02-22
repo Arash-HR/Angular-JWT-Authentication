f4ec2e6b583516b1793318c31a1b58c690ed6313
Arash-HR <arashhosseinirezaei@gmail.com>
2022-02-22 20:45:34 +0330

    create post service

---
+++ src/app/services/post.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Post } from '../interfaces/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${environment.apiurl}/posts`);
+  }
+}
